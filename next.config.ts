9d37567759836ad991635be72c9930ac90fb032f
iii2kk <temp345@hotmail.co.jp>
2026-04-21 23:01:30 +0900

    不要なログを削除

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  logging: {
+    browserToTerminal: false,
+  },
+  experimental: {
+    mcpServer: false,
+  },
   transpilePackages: ["three"],
 };
 
